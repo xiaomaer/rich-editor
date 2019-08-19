fe41768f4f7ffc7bab372af450edf997d5c49a02
马美英 <mameiying@meituan.com>
2019-08-19 12:11:22 +0800

    feat(config): 验证项目规范

--- src/components/Storage/index.jsx
+++ src/components/Storage/index.jsx
@@ -7,59 +7,59 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Storage extends Component {
-    static propTypes = {
-        render: PropTypes.func.isRequired
+  static propTypes = {
+    render: PropTypes.func.isRequired
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      localStorageAvailable: false
     };
-    constructor(props) {
-        super(props);
-        this.state = {
-            localStorageAvailable: false
-        };
+  }
+  componentDidMount() {
+    this.checkLocalStorageExists();
+  }
+  checkLocalStorageExists() {
+    const testKey = 'test';
+    try {
+      localStorage.setItem(testKey, testKey);
+      localStorage.removeItem(testKey);
+      this.setState({
+        localStorageAvailable: true
+      });
+    } catch (err) {
+      this.setState({
+        localStorageAvailable: false
+      });
     }
-    componentDidMount() {
-        this.checkLocalStorageExists();
+  }
+  load = (key) => {
+    if (this.state.localStorageAvailable) {
+      return localStorage.getItem(key);
     }
-    checkLocalStorageExists() {
-        const testKey = 'test';
-        try {
-            localStorage.setItem(testKey, testKey);
-            localStorage.removeItem(testKey);
-            this.setState({
-                localStorageAvailable: true
-            });
-        } catch (err) {
-            this.setState({
-                localStorageAvailable: false
-            });
-        }
+    return null;
+  };
+  save = (key, data) => {
+    if (this.state.localStorageAvailable) {
+      localStorage.setItem(key, data);
     }
-    load = (key) => {
-        if (this.state.localStorageAvailable) {
-            return localStorage.getItem(key);
-        }
-        return null;
-    };
-    save = (key, data) => {
-        if (this.state.localStorageAvailable) {
-            localStorage.setItem(key, data);
-        }
-    };
-    remove = (key) => {
-        if (this.state.localStorageAvailable) {
-            localStorage.removeItem(key);
-        }
-    };
-    render() {
-        return (
-            <span>
-                {this.props.render({
-                    load: this.load,
-                    save: this.save,
-                    remove: this.remove
-                })}
-            </span>
-        );
+  };
+  remove = (key) => {
+    if (this.state.localStorageAvailable) {
+      localStorage.removeItem(key);
     }
+  };
+  render() {
+    return (
+      <span>
+        {this.props.render({
+          load: this.load,
+          save: this.save,
+          remove: this.remove
+        })}
+      </span>
+    );
+  }
 }
 
 export default Storage;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -5,12 +5,12 @@ import Home from '../pages/Home';
 const Login = () => import('../pages/Login');
 
 const App = () => (
-    <Router>
-        <Switch>
-            <Route path="/login" component={Login} />
-            {/* 这样就可以直接访问子路由了 */}
-            <Home />
-        </Switch>
-    </Router>
+  <Router>
+    <Switch>
+      <Route path="/login" component={Login} />
+      {/* 这样就可以直接访问子路由了 */}
+      <Home />
+    </Switch>
+  </Router>
 );
 export default App;
